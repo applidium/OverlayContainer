72343348e4583a22b33a6761b910f4fdca99a7c2
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2018-11-29 10:14:55 +0100

    Add defaults to delegate methods

---
+++ OverlayContainer/Classes/Extensions/OverlayContainerViewControllerDelegate+Defaults.swift
@@ -0,0 +1,45 @@
+//
+//  OverlayContainerViewControllerDelegate+Defaults.swift
+//  OverlayContainer
+//
+//  Created by Gaétan Zanella on 23/11/2018.
+//
+
+import Foundation
+
+public extension OverlayContainerViewControllerDelegate {
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        transitionningDelegateForOverlay overlayViewController: UIViewController) -> OverlayTransitioningDelegate? {
+        return nil
+    }
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        scrollViewDrivingOverlay overlayViewController: UIViewController) -> UIScrollView? {
+        return nil
+    }
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        shouldStartTranslationAt point: CGPoint) -> Bool {
+        return true
+    }
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        shouldStartDraggingOverlay overlayViewController: UIViewController,
+                                        at point: CGPoint,
+                                        in coordinateSpace: UICoordinateSpace) -> Bool {
+        return true
+    }
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        overlayTranslationFunctionForOverlay overlayViewController: UIViewController) -> OverlayTranslationFunction? {
+        return nil
+    }
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        didDragOverlay overlayViewController: UIViewController,
+                                        toHeight height: CGFloat) {}
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        willEndReachingNotchAt index: Int,
+                                        transitionCoordinator: OverlayContainerTransitionCoordinator) {}
+}
