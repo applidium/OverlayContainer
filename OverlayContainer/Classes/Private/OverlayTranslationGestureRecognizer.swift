635a6e3cd1c3ff983da7dbec73654d79b1117929
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2018-11-30 11:29:36 +0100

    Trigger pan gesture immediately

--- OverlayContainer/Classes/Private/OverlayTranslationGestureRecognizer.swift
+++ OverlayContainer/Classes/Private/OverlayTranslationGestureRecognizer.swift
@@ -8,8 +8,18 @@
 import UIKit
 
 class OverlayTranslationGestureRecognizer: UIPanGestureRecognizer {
+
+    // MARK: - Public
+
     func cancel() {
         isEnabled = false
         isEnabled = true
+    }
+
+    // MARK: - UIGestureRecognizer
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesBegan(touches, with: event)
+        state = .began
     }
 }
