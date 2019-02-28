52d78b6fba830040e3444a16c6ff77fc003c9868
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2019-02-28 10:21:54 +0100

    Add notches invalidation test

--- Tests/OverlayNotchHeightTest.swift
+++ Tests/OverlayNotchHeightTest.swift
@@ -10,43 +10,21 @@ import Nimble
 import Quick
 import OverlayContainer
 
-class OverlayNotchHeightTest: QuickSpec, OverlayContainerViewControllerDelegate {
+private enum Notch: Int, CaseIterable {
+    case minimum, medium, maximum
+}
 
-    enum Notch: Int, CaseIterable {
-        case minimum, medium, maximum
-    }
+private enum NewNotch: Int, CaseIterable {
+    case minimum, medium
+}
 
-    // MARK: - QuickSpec
+private class OverlayContainerDelegateImplementation: OverlayContainerViewControllerDelegate {
 
-    override func spec() {
-        let overlayContainer = OverlayContainerViewController()
-        overlayContainer.loadViewIfNeeded()
-        overlayContainer.view.frame = CGRect(origin: .zero, size: CGSize(width: 300, height: 300))
-        let overlay = UIViewController()
-        overlayContainer.delegate = self
-        overlayContainer.viewControllers = [overlay]
-        it("should move the overlay to the specified notch") {
-            Notch.allCases.forEach { notch in
-                overlayContainer.moveOverlay(toNotchAt: notch.rawValue, animated: false)
-                overlayContainer.view.layoutIfNeeded()
-                expect(overlay.view.frame.height).to(equal(self.height(for: notch)))
-            }
-        }
-    }
+    var usesNewNotches = false
 
-    // MARK: - OverlayContainerViewControllerDelegate
+    // MARK: - Public
 
-    func numberOfNotches(in containerViewController: OverlayContainerViewController) -> Int {
-        return Notch.allCases.count
-    }
-
-    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
-                                        heightForNotchAt index: Int,
-                                        availableSpace: CGFloat) -> CGFloat {
-        return height(for: Notch.allCases[index])
-    }
-
-    private func height(for notch: Notch) -> CGFloat {
+    func height(for notch: Notch) -> CGFloat {
         switch notch {
         case .minimum:
             return 0
@@ -54,6 +32,74 @@ class OverlayNotchHeightTest: QuickSpec, OverlayContainerViewControllerDelegate 
             return 50
         case .maximum:
             return 100
+        }
+    }
+
+    func height(forNew notch: NewNotch) -> CGFloat {
+        switch notch {
+        case .minimum:
+            return 10
+        case .medium:
+            return 200
+        }
+    }
+
+    // MARK: - OverlayContainerViewControllerDelegate
+
+    func numberOfNotches(in containerViewController: OverlayContainerViewController) -> Int {
+        if usesNewNotches {
+            return NewNotch.allCases.count
+        }
+        return Notch.allCases.count
+    }
+
+    func overlayContainerViewController(_ containerViewController: OverlayContainerViewController,
+                                        heightForNotchAt index: Int,
+                                        availableSpace: CGFloat) -> CGFloat {
+        if usesNewNotches {
+            return height(forNew: NewNotch.allCases[index])
+        }
+        return height(for: Notch.allCases[index])
+    }
+}
+
+class OverlayNotchHeightTest: QuickSpec {
+
+    // MARK: - QuickSpec
+
+    override func spec() {
+        var overlayContainer: OverlayContainerViewController!
+        var delegate: OverlayContainerDelegateImplementation!
+        var overlay: UIViewController!
+        beforeEach {
+            overlayContainer = OverlayContainerViewController()
+            delegate = OverlayContainerDelegateImplementation()
+            overlay = UIViewController()
+            overlayContainer.loadViewIfNeeded()
+            overlayContainer.view.frame = CGRect(origin: .zero, size: CGSize(width: 300, height: 300))
+            overlayContainer.delegate = delegate
+            overlayContainer.viewControllers = [overlay]
+        }
+        it("should move the overlay to the specified notch") {
+            Notch.allCases.forEach { notch in
+                overlayContainer.moveOverlay(toNotchAt: notch.rawValue, animated: false)
+                overlayContainer.view.layoutIfNeeded()
+                expect(overlay.view.frame.height).to(equal(delegate.height(for: notch)))
+            }
+        }
+        it("should invalidate the current notches") {
+            Notch.allCases.forEach { notch in
+                overlayContainer.moveOverlay(toNotchAt: notch.rawValue, animated: false)
+                overlayContainer.view.layoutIfNeeded()
+                expect(overlay.view.frame.height).to(equal(delegate.height(for: notch)))
+            }
+            delegate.usesNewNotches = true
+            overlayContainer.invalidateNotchHeights()
+            NewNotch.allCases.forEach { notch in
+                overlayContainer.moveOverlay(toNotchAt: notch.rawValue, animated: false)
+                overlayContainer.view.layoutIfNeeded()
+                expect(overlay.view.frame.height).to(equal(delegate.height(forNew: notch)))
+            }
         }
     }
 }
