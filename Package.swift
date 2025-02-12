7cb7a47f780dd3412fcf5f61f6127648d5e63019
Zeno Koller <koller@ubique.ch>
2025-02-12 17:11:57 +0100

    Update Quick to 4.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "3.1.0"),
+        .package(url: "https://github.com/Quick/Quick", from: "4.0.0"),
     ],
     targets: [
         .target(
